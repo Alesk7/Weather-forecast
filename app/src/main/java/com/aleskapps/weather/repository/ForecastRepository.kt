cd6197fecd9809cc77ccb270b59ed993331356c9
Alexey <aleskapps@gmail.com>
2019-07-30 12:11:34 +0300

    Implemented forecast repository

--- app/src/main/java/com/aleskapps/weather/repository/ForecastRepository.kt
+++ app/src/main/java/com/aleskapps/weather/repository/ForecastRepository.kt
@@ -1,8 +1,20 @@
 package com.aleskapps.weather.repository
 
 import com.aleskapps.weather.entities.Forecast
+import com.aleskapps.weather.retrofit.getForecastService
 import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
-interface ForecastRepository {
-    fun getFiveDaysForecastByCoordinats(latitude: Double, longitude: Double): Single<List<Forecast>>
+class ForecastRepository (private val apiKey: String) {
+
+    private val forecastService = getForecastService()
+
+    fun getFiveDaysForecastByCoordinats(latitude: Double, longitude: Double): Single<List<Forecast>> {
+        return forecastService.getFiveDaysForecastByCoordinats(latitude, longitude, apiKey)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .map { it.list }
+    }
+
 }
